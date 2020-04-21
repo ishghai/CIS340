0b54d66cc9a77acd20281f700b924885b118ae47
ishghai <63438889+ishghai@users.noreply.github.com>
2020-04-21 06:39:01 +0000

    Fourth Project

---
+++ MyFourthProject/App copy.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { Text, View } from 'react-native';
+
+export default class MyApp extends Component {
+  render() {
+  return (
+    <View style={{padding: 60}}>
+      <Text>Hello this is Ish CIS340 student!</Text>
+      <Text>I like using the class component!</Text>
+    </View>
+  );
+}
+}
